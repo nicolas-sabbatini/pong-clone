c390eeff561e8436faea334ecfb7fc18f7eeb642
Nicolas <nicolas.sabbatini.95@gmail.com>
2019-12-22 03:14:48 -0300

    vsync

--- src/conf.lua
+++ src/conf.lua
@@ -1,5 +1,6 @@
 function love.conf(t)
-  t.window.title = "Pong"         -- The window title (string)
-  t.window.width = 1280            -- The window width (number)
-  t.window.height = 720           -- The window height (number)
+  t.window.title = "Pong"  -- The window title (string)
+  t.window.width = 1280    -- The window width (number)
+  t.window.height = 720    -- The window height (number)
+  t.window.vsync = 1       -- Vertical sync mode (number)
 end
